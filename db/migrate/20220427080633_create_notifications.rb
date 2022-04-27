6110d86277881118ea08d1e0453b24773c632b73
sakamoto-waka <wakanmhl@gmail.com>
2022-04-27 08:13:20 +0000

    [update] migration file for notifications

--- db/migrate/20220427080633_create_notifications.rb
+++ db/migrate/20220427080633_create_notifications.rb
@@ -1,8 +1,20 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
+      t.integer :visitor_id, null:false
+      t.integer :visited_id, null:false
+      t.integer :book_id
+      t.integer :book_comment_id
+      t.string :action, default: '', null: false
+      # 通知を確認したか
+      t.boolean :checked, default: false, null: false
 
       t.timestamps
     end
+    
+    add_index :notifications, :visitor_id
+    add_index :notifications, :visited_id
+    add_index :notifications, :book_id
+    add_index :notifications, :book_comment_id
   end
 end
