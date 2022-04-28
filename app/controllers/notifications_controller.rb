205688af91fade062340429a8bbb0dd4f0abb86d
sakamoto-waka <wakanmhl@gmail.com>
2022-04-28 04:05:16 +0000

    [fix] _notification_except_me notification.adtion to 'following'

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
     # 未通知を取得、未確認を確認済みにする処理
     @notifications = current_user.passive_notifications
     @notifications.where(checked: false).each do |notification|
-      notification.update_attribute(checked: true)
+      notification.update_attribute(:checked, true)
     end
     # @notifications_except_me = notifications.where.not(visitor_id: current_user.id)
   end
